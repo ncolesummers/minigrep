c9dd0aa0e238f1db52101bb97445bbc12197681f
Cole Summers <nsummers72@gmail.com>
2021-03-20 16:40:01 -0700

    Done with Chapter 12

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -13,8 +15,10 @@ impl Config {
       }
       let query = args[1].clone();
       let filename = args[2].clone();
+
+      let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
   
-      Ok(Config { query, filename })
+      Ok(Config { query, filename, case_sensitive, } )
   }
 }
 
@@ -22,7 +26,99 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename)?;
 
+  let results = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_case_insensitive(&config.query, &contents)
+  };
 
-  println!("With text:\n{}", contents);
+  for line in results {
+    println!("{}", line);
+  }
+
   Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+pub fn search_case_insensitive<'a>(
+  query: &str,
+  contents: &'a str,
+) -> Vec<&'a str> {
+  
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents)
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   fn poem() {
+//     let config = Config::new("the", "poem.txt");
+//     let result = run(config);
+//     assert!(
+//       result.contains("With text:"),
+//       "wrong args likely",
+//       result
+//     );
+//   }
+// }
